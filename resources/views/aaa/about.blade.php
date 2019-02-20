c25ff6ce360eef162b438d7973cc9228d131d7ab
VukasinVujic <vukasinvujic021@gmail.com>
2019-02-20 10:34:06 +0100

    zadatak 2

--- resources/views/aaa/about.blade.php
+++ resources/views/aaa/about.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    welcome to cars Project
+                    about us
                 </div>
 
                 <div class="links">
