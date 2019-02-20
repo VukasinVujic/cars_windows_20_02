5af1f5ebdfcb6949358a17966bc2af59bb193b84
VukasinVujic <vukasinvujic021@gmail.com>
2019-02-20 15:52:22 +0100

    zadatak 8

--- resources/views/aaa/index.blade.php
+++ resources/views/aaa/index.blade.php
@@ -1,19 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.master')
 
-    @foreach ($cars as $car)
 
-    <li><a href="{{ 'http://localhost/VIVIFY/napredni/Laravel/radOdkuce_20_02_cars/cars_20_02/public/cars/' . $car->id }}"> {{ $car->title }} </a></li>
-        
-    @endforeach
-    <br>
-    <a href="http://localhost/VIVIFY/napredni/Laravel/radOdkuce_20_02_cars/cars_20_02/public/">Home</a>
-</body>
-</html>
+
+@section('title')
+
+INDEX BLADE
+    
+@endsection
+
+
+@section('content')
+
+
+@foreach ($cars as $car)
+
+<li><a href="{{ 'http://localhost/VIVIFY/napredni/Laravel/radOdkuce_20_02_cars/cars_20_02/public/cars/' . $car->id }}"> {{ $car->title }} </a></li>
+    
+@endforeach
+<br>
+<a href="http://localhost/VIVIFY/napredni/Laravel/radOdkuce_20_02_cars/cars_20_02/public/">Home</a>
+
+
+    
+@endsection
+
+
